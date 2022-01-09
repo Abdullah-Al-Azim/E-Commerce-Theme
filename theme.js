66e339fbd5146472dfded4d733d9e28cad0acb64
Abdullah Al Azim <82142840+Abdullah-Al-Azim@users.noreply.github.com>
2022-01-09 16:08:17 +0600

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,73 +1,153 @@
-// search bar
+$(document).ready(() => {
+  $(".main_____wrods____wrap").slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow:
+      '<button class="slick__nextBtn"><span class="iconify text-sky-600 text-3xl" data-icon="bi:arrow-right-circle-fill"></span></button>',
+    prevArrow:
+      '<button class="slick__prevBtn"><span class="iconify text-sky-600 text-3xl" data-icon="bi:arrow-left-circle-fill"></span></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
-const searchIcon = document.getElementById("search______bar");
-const cross = document.getElementById("cross_____icon");
-const searchBar = document.getElementById("main___search___bar");
+  // Category slider
+  // $(".category____slider").slick({
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 800,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  //   nextArrow:
+  //     '<button class="slick__nextBtn"><span class="iconify text-sky-600 text-3xl" data-icon="bi:arrow-right-circle-fill"></span></button>',
+  //   prevArrow:
+  //     '<button class="slick__prevBtn"><span class="iconify text-sky-600 text-3xl" data-icon="bi:arrow-left-circle-fill"></span></button>',
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         infinite: true,
+  //         dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // });
 
-searchIcon.addEventListener("click", function (e) {
-  searchBar.classList.add("show___searchbar");
-  document
-    .querySelector(".admin____overlay")
-    .classList.add("show__adminOverlay");
-});
+  // search bar
+  const searchIcon = document.getElementById("search______bar");
+  const cross = document.getElementById("cross_____icon");
+  const searchBar = document.getElementById("main___search___bar");
 
-cross.addEventListener("click", () => {
-  searchBar.classList.remove("show___searchbar");
-  document
-    .querySelector(".admin____overlay")
-    .classList.remove("show__adminOverlay");
-});
+  searchIcon.addEventListener("click", function (e) {
+    searchBar.classList.add("show___searchbar");
+    document
+      .querySelector(".admin____overlay")
+      .classList.add("show__adminOverlay");
+  });
 
-document.addEventListener("click", (event) => {
-  if (event.target.closest("#search______bar")) return;
-  if (event.target.closest("#main___search___bar")) return;
-
-  if (event.target.classList[1] === "show__adminOverlay") {
+  cross.addEventListener("click", () => {
+    searchBar.classList.remove("show___searchbar");
     document
       .querySelector(".admin____overlay")
       .classList.remove("show__adminOverlay");
-  }
+  });
 
-  searchBar.classList.remove("show___searchbar");
-});
+  document.addEventListener("click", (event) => {
+    if (event.target.closest("#search______bar")) return;
+    if (event.target.closest("#main___search___bar")) return;
 
-// category
-// const homeCat = document.getElementById("home___cat");
-// const homeMaainCat = document.querySelector(".main____cat___div");
-// homeCat.style.display = "none";
+    if (event.target.classList[1] === "show__adminOverlay") {
+      document
+        .querySelector(".admin____overlay")
+        .classList.remove("show__adminOverlay");
+    }
 
-// function homeCategory() {
-//   homeCat.style.display = "flex";
-// }
+    searchBar.classList.remove("show___searchbar");
+  });
 
-// function homeLeave() {
-//   homeCat.style.display = "none";
-// }
+  // category
+  // const homeCat = document.getElementById("home___cat");
+  // const homeMaainCat = document.querySelector(".main____cat___div");
+  // homeCat.style.display = "none";
 
-// const shop = document.getElementById("shop__cat");
-// shop.style.display = "none";
+  // function homeCategory() {
+  //   homeCat.style.display = "flex";
+  // }
 
-// function shopCategory() {
-//   shop.style.display = "flex";
-//   homeMaainCat.style.zIndex = "99999";
-// }
-// function shopLeave() {
-//   shop.style.display = "none";
-// }
+  // function homeLeave() {
+  //   homeCat.style.display = "none";
+  // }
 
-// const page = document.getElementById("page__cat");
-// page.style.display = "none";
-// function pageCategory() {
-//   page.style.display = "flex";
-// }
+  // const shop = document.getElementById("shop__cat");
+  // shop.style.display = "none";
 
-// function pageLeave() {
-//   page.style.display = "none";
-// }
+  // function shopCategory() {
+  //   shop.style.display = "flex";
+  //   homeMaainCat.style.zIndex = "99999";
+  // }
+  // function shopLeave() {
+  //   shop.style.display = "none";
+  // }
 
-// slider frindly
+  // const page = document.getElementById("page__cat");
+  // page.style.display = "none";
+  // function pageCategory() {
+  //   page.style.display = "flex";
+  // }
 
-// full screen grid
-$(".snake").snakeify({
-  speed: 200,
+  // function pageLeave() {
+  //   page.style.display = "none";
+  // }
+
+  // slider frindly
+
+  // full screen grid
+  $(".snake").snakeify({
+    speed: 200,
+  });
 });
