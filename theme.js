0e35cfad82ce00ec7ab1e51c20a6f44747c6630a
azim <azeemzeehan3@gmail.com>
2022-01-10 01:54:26 +0600

    E-commerce

--- theme.js
+++ theme.js
@@ -30,3 +30,44 @@ document.addEventListener("click", (event) => {
 
   searchBar.classList.remove("show___searchbar");
 });
+
+// category
+// const homeCat = document.getElementById("home___cat");
+// const homeMaainCat = document.querySelector(".main____cat___div");
+// homeCat.style.display = "none";
+
+// function homeCategory() {
+//   homeCat.style.display = "flex";
+// }
+
+// function homeLeave() {
+//   homeCat.style.display = "none";
+// }
+
+// const shop = document.getElementById("shop__cat");
+// shop.style.display = "none";
+
+// function shopCategory() {
+//   shop.style.display = "flex";
+//   homeMaainCat.style.zIndex = "99999";
+// }
+// function shopLeave() {
+//   shop.style.display = "none";
+// }
+
+// const page = document.getElementById("page__cat");
+// page.style.display = "none";
+// function pageCategory() {
+//   page.style.display = "flex";
+// }
+
+// function pageLeave() {
+//   page.style.display = "none";
+// }
+
+// slider frindly
+
+// full screen grid
+$(".snake").snakeify({
+  speed: 200,
+});
